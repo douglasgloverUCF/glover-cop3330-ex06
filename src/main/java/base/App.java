49c5753ccd015b04990f4787f5b997b22107dc22
Douglas Glover <douglasglover3@knights.ucf.edu>
2021-06-05 19:12:23 -0400

    First Iteration

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -3,6 +3,8 @@
  *  Copyright 2021 Douglas Glover
  */
 package base;
+import java.util.Calendar;
+import java.util.Scanner;
 /*
 Your computer knows what the current year is, which means you can incorporate that into your programs.
 You just have to figure out how your programming language can provide you with that information.
@@ -27,5 +29,31 @@ Challenge
 Handle situations where the program returns a negative number by stating that the user can already retire.
  */
 public class App {
-
+    Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        App myApp = new App();
+        int age = myApp.getAge();
+        int retire = myApp.getRetire();
+        myApp.untilRetire(age, retire);
+    }
+    int getAge()
+    {
+        System.out.print("What is your current age? ");
+        String input = in.nextLine();
+        return Integer.parseInt(input);
+    }
+    int getRetire()
+    {
+        System.out.print("At what age would you like to retire? ");
+        String input = in.nextLine();
+        return Integer.parseInt(input);
+    }
+    void untilRetire(int age, int retire)
+    {
+        int difference = retire - age;
+        System.out.println("You have " + difference + " years left until you can retire.");
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int retireYear = year + difference;
+        System.out.print("It's " + year + ", so you can retire in " + retireYear + ".");
+    }
 }
